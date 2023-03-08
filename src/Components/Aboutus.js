20c438633f1c2905d18efbb0e8cf9c671c702794
ROHEETNARSINGHA <narsinghakhadkar11@gmail.com>
2023-03-08 18:53:08 +0545

    Random Changes

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -16,12 +16,8 @@ export default function Aboutus() {
                     </div>
                     <div className="col-md-6">
                         <h5>Parts for agricultural tractors Agrofarm</h5>
-                        <p>N. Because we have a well stocked warehouseme day by UPS courrier company. 
-                        N. Because we have a well stocked warehouseme day by UPS courrier company. 
-                        veryN. Because we have a well stocked warehouseme day by UPS courrier company. 
-                        N. Because we have a well stocked warehouseme day by UPS courrier company. 
-                        N. Because we have a well stocked warehouseme day by UPS courrier company. 
-                        N. Because we have a well stocked warehouseme day by UPS courrier company. 
+                        <p>N. Our next goal is to provide options for buying equipments and many more. The aim here is to provide the users a single place where they can use different features like live weather forecast, learn about modern techniques for improving the cultivation process. We are also planning
+                            to add a new feature where the farmers or the users can raise their questions and talk with Agrologist.   
                         </p>
                         </div>
                 </div>
@@ -36,15 +32,15 @@ export default function Aboutus() {
                                 <p>In our offer you can find second hand parts from dismantled tractors, new ones and replacements.
                                      Because we have a well stocked warehouse we can deliver all parts very quickly. Of course they are
                                       carefully checked before dispatch. We do our best to make sure our assortment is reliable. We offer a
-                                      n second-hand partse warehparts very quickly. Of course they are carefully checked before dispatch. We do our best to make sure our assortment is reliable. We offer a two-week quarantee on second-hand parts. The orders placed before 2 p.m. are processed and sent the same day by UPS courrier company. </p>
+                                      n second-hand partse warehparts very quickly. Of course they are carefully checked before dispatch. We do our best to make sure our assortment is reliable. We offer a two-week quarantee on second-hand parts. The orders placed before 2 p.m. are packaged and delivered shortly. </p>
                                 </div>
                         </div>
 
                     <div className="col-md-6 ">
                         <div className='missionabout'>
                         <h5>Vision</h5>
-                        <p>To utilize local resources of Nepal in realityand let the world see for what really they are meant. Uniyeko Shop is committed to raise thestandard of living of rural and urban Nepalese by providing them with anopportunity to earn an income.  In this 21st century, work as abridge to access between products and potential customers and save their time.
-                             Allthe products seen in the website are ready to dispatch materials so we justneed the time for shipping only.</p>
+                        <p>To utilize local resources of Nepal in reality and provide a single platform for multiple purposes regarding Agricultural sector and Agro products. Krishak Sathi is committed to raise the standard of living of rural and urban Nepalese by providing them with an opportunity to easily buy fresh vegetables.  It work as a bridge to access between products and potential customers and save their time.
+                             All the products seen in the website are ready to dispatch materials so we just need the time delivering the products.</p>
                     </div></div>
 
                 </div>
